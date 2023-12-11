7dadf3bf7e283095830d4c18e9f9390174dcc370
Ansear <sebastianarciniegas2003@gmail.com>
2023-12-11 17:06:20 -0500

    unidad de trabajo check

--- Application/UnitOfWork/UnitOfWork.cs
+++ Application/UnitOfWork/UnitOfWork.cs
@@ -13,6 +13,28 @@ public class UnitOfWork : IUnitOfWork, IDisposable
     private readonly SecurityContext _context;
     private IRolRepository _roles;
     private IUserRepository _users;
+
+    private IAddressperson _addresspeople;
+   
+    private ICategoryperson _categorypeople; 
+    private ICity _cities;
+   
+    private IContactperson _Contactpeople; 
+    private IContract _Contracts;
+   
+    private ICountry _Countries; 
+    private IPerson _People;
+   
+    private IProgramming _Programmings; 
+    private IRegion _Regions;
+   
+    private IShift _Shifts; 
+    private IStatus _Statuses;
+   
+    private ITypeaddress _Typeaddresses; 
+    private ITypecontact _Typecontacts;
+   
+    private ITypeperson _Typepeople; 
   
     public UnitOfWork(SecurityContext context)
     {
@@ -41,6 +63,168 @@ public class UnitOfWork : IUnitOfWork, IDisposable
             return _users;
         }
     }
+    public IAddressperson Addresspeople
+    {
+        get
+        {
+            if (_addresspeople == null)
+            {
+                _addresspeople = new AddresspersonRepository(_context);
+            }
+            return _addresspeople;
+        }
+    }
+    public ICategoryperson Categorypeople
+    {
+        get
+        {
+            if (_categorypeople == null)
+            {
+                _categorypeople = new CategorypersonRepository(_context);
+            }
+            return _categorypeople;
+        }
+    }
+    public ICity Cities
+    {
+        get
+        {
+            if (_cities == null)
+            {
+                _cities = new CityRepository(_context);
+            }
+            return _cities;
+        }
+    }
+    public IContactperson Contactpeople
+    {
+        get
+        {
+            if (_Contactpeople == null)
+            {
+                _Contactpeople = new ContactpersonRepository(_context);
+            }
+            return _Contactpeople;
+        }
+    }
+    public IContract Contracts
+    {
+        get
+        {
+            if (_Contracts == null)
+            {
+                _Contracts = new ContractRepository(_context);
+            }
+            return _Contracts;
+        }
+    }
+    public ICountry Countries
+    {
+        get
+        {
+            if (_Countries == null)
+            {
+                _Countries = new CountryRepository(_context);
+            }
+            return _Countries;
+        }
+    }
+    public IPerson People
+    {
+        get
+        {
+            if (_People == null)
+            {
+                _People = new PersonRepository(_context);
+            }
+            return _People;
+        }
+    }
+    public IProgramming Programmings
+    {
+        get
+        {
+            if (_Programmings == null)
+            {
+                _Programmings = new ProgrammingRepository(_context);
+            }
+            return _Programmings;
+        }
+    }
+    public IRegion Regions
+    {
+        get
+        {
+            if (_Regions == null)
+            {
+                _Regions = new RegionRepository(_context);
+            }
+            return _Regions;
+        }
+    }
+    public IShift Shifts
+    {
+        get
+        {
+            if (_Shifts == null)
+            {
+                _Shifts = new ShiftRepository(_context);
+            }
+            return _Shifts;
+        }
+    }
+
+    public IStatus Statuses
+    {
+        get
+        {
+            if (_Statuses == null)
+            {
+                _Statuses = new StatusRepository(_context);
+            }
+            return _Statuses;
+        }
+    }
+    public ITypeaddress Typeaddresses
+    {
+        get
+        {
+            if (_Typeaddresses == null)
+            {
+                _Typeaddresses = new TypeaddressRepository(_context);
+            }
+            return _Typeaddresses;
+        }
+    }
+
+
+    public ITypecontact Typecontacts
+    {
+        get
+        {
+            if (_Typecontacts == null)
+            {
+                _Typecontacts = new TypecontactRepository(_context);
+            }
+            return _Typecontacts;
+        }
+    }
+
+
+    public ITypeperson Typepeople
+    {
+        get
+        {
+            if (_Typepeople == null)
+            {
+                _Typepeople = new TypepersonRepository(_context);
+            }
+            return _Typepeople;
+        }
+    }
+
+
+
     
     public async Task<int> SaveAsync()
     {
--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -9,5 +9,21 @@ public interface IUnitOfWork
 {
     IRolRepository Roles { get; }
     IUserRepository Users { get; }
+
+    IAddressperson Addresspeople {get;}
+    ICategoryperson Categorypeople {get;}
+    ICity Cities  {get;}
+    IContactperson Contactpeople {get;}
+    IContract Contracts {get;}
+    ICountry Countries {get;}
+    IPerson People {get;}
+    IProgramming Programmings {get;}
+    IRegion Regions {get;}
+    IShift Shifts {get;}
+
+    IStatus Statuses {get;}
+    ITypeaddress Typeaddresses {get;}
+    ITypecontact Typecontacts {get;}
+    ITypeperson Typepeople {get;}
     Task<int> SaveAsync();
 }
